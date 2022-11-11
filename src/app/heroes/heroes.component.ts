27d96671e473bb77c1c0ebb3a92b554bfbbbee1b
Azdevpp <azdevpp@gmail.com>
2022-11-11 21:45:47 +0200

    getHeroFunc

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,13 +13,20 @@ export class HeroesComponent implements OnInit {
   //   id: 1,
   //   name: 'Windstorm',
   // };
-  heroes = HEROES;
+  // heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
